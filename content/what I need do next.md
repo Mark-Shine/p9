ee431a9cd1d3931c6b332a860155dc1dd9e59f47
Shiyi <wb-shangzhihai@taobao.com>
2014-07-31 19:26:31 +0800

    initial commit

---
+++ content/what I need do next.md
@@ -0,0 +1,13 @@
+Date: 2014-06-22  
+Title:   
+Published: true  
+Type: post  
+Excerpt: 
+slug: what-i-need-to-do
+近期要做的事儿：
+	
+* 学习 Python 函数式编程及概念 [链接](http://www.cnblogs.com/huxi/archive/2011/06/18/2084316.html)
+* 学习 LISP 从而深入体验函数式 [链接](http://www.cs.sfu.ca/CourseCentral/310/pwfong/Lisp/)
+* 开发微信接口 完成关注事件的接受和处理
+* 学习coursera上市场营销概论
+
---
+++ pelicanconf.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+THEME = "/Users/pison/Project/blog/pelican-themes/jesuislibre"
+AUTHOR = 'P9'
+SITENAME = 'Blog'
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+#LINKS = (('Pelican', 'http://getpelican.com/'),
+#         ('Python.org', 'http://python.org/'),
+#         ('Jinja2', 'http://jinja.pocoo.org/'),
+#         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+
+LINKS = ((u'Home', u'/'), (u'archives', u'/archives.html'), (u'tags', u'/tags.html'), (u'A propos', u'http://bruno.adele.im'))
+SITESUBTITLE = u'subtitle'
+DEFAULT_LANG = u'en'
+SITEURL = ''
+SOCIAL = ((u'adele', u'http://adele.im'), (u'feed', u'http://feeds.feedburner.com/FamilleDeGeek'), (u'twitter', u'https://twitter.com/_Markpapa'), (u'google+', u'https://plus.google.com/113567571752504189885'), (u'blog', u'http://blog.a-2.co'), (u'facebook', u'http://www.facebook.com/zhihai.shang'), )
+#FEED_DOMAIN = u'http://blog.jesuislibre.org'
+#FEED_ALL_ATOM = u'feed.atom'
+#DISQUS_SITENAME = u'blogdejesuislibreorg'
+DEFAULT_PAGINATION = 10
+#GITHUB_BLOG_SITE = u'https://github.com/badele/blog.jesuislibre.org'
